7369557fc07db20a1734a2aa42dedbaaab763cb5
chanwoo <mdsoo55828@gmail.com>
2024-11-16 15:23:47 +0900

    초기 스프링 부트 세팅

---
+++ src/main/java/wow/grmr/GrmrApplication.java
@@ -0,0 +1,14 @@
+package wow.grmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrmrApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/wow/grmr/GrmrApplicationTests.java
@@ -0,0 +1,13 @@
+package wow.grmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
